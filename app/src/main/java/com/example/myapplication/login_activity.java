01b10a7fa9c8f4e15c62d8b4b6beefb09c5eee0d
RUMEL <naimurrahman1046@gmail.com>
2023-02-18 15:17:49 +0600

    There are Home Activity

--- app/src/main/java/com/example/myapplication/login_activity.java
+++ app/src/main/java/com/example/myapplication/login_activity.java
@@ -8,6 +8,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
