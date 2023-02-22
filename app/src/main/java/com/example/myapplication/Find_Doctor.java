e881d684ab1dba6fe5457cfdd593e6d5bf207be7
RUMEL <naimurrahman1046@gmail.com>
2023-02-22 08:29:10 +0600

    In this section Exit button activated

--- app/src/main/java/com/example/myapplication/Find_Doctor.java
+++ app/src/main/java/com/example/myapplication/Find_Doctor.java
@@ -1,17 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Find_Doctor extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_find_doctor);
     }
-
 }
--- app/src/main/java/com/example/myapplication/Home_Activity.java
+++ app/src/main/java/com/example/myapplication/Home_Activity.java
@@ -1,8 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Home_Activity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class Home_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        SharedPreferences sp= getSharedPreferences("Simple_Data", Context.MODE_PRIVATE);
+        String username=sp.getString("username","").toString();
+        Toast.makeText(this, ""+username, Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor ed = sp.edit();
+                ed.clear();
+                ed.apply();
+                startActivity(new Intent(Home_Activity.this,login_activity.class));
+            }
+        });
+
     }
 }
