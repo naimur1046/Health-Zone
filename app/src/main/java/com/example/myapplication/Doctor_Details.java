1e3d0725f764b07d3d90e2d7aeca0353740da814
RUMEL <naimurrahman1046@gmail.com>
2023-02-23 13:15:46 +0600

    In this section I included users

--- app/src/main/java/com/example/myapplication/Doctor_Details.java
+++ app/src/main/java/com/example/myapplication/Doctor_Details.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Doctor_Details extends AppCompatActivity
 {
-     TextView textView;
+     TextView title;
+     Button exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +20,15 @@ public class Doctor_Details extends AppCompatActivity
         Intent intent = getIntent();
         String name_of_section= intent.getStringExtra("title");
 
-        textView= findViewById(R.id.title_text);
-        textView.setText(""+name_of_section);
-
+        title= findViewById(R.id.title_text);
+        title.setText(""+name_of_section);
+        exit = findViewById(R.id.doctor_details_exit_button);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity( new Intent(Doctor_Details.this,Find_Doctor.class));
+            }
+        });
     }
 }
