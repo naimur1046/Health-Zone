7604dd88cdb90002c86f395e5d2b98bfa26e5ba1
RUMEL <naimurrahman1046@gmail.com>
2023-03-10 19:35:42 +0600

    In this section I have completed my firebase push option

--- app/src/main/java/com/example/myapplication/Register_Activity.java
+++ app/src/main/java/com/example/myapplication/Register_Activity.java
@@ -9,14 +9,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Register_Activity extends AppCompatActivity {
     EditText edName,edPassword,edConfirmPassword,edEmail,edPhoneNumber;
     Button btn;
     TextView bottomText;
+
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
         edName=findViewById(R.id.userName);
         edPassword=findViewById(R.id.password);
         edConfirmPassword=findViewById(R.id.confirm_password);
@@ -24,15 +31,19 @@ public class Register_Activity extends AppCompatActivity {
         edPhoneNumber=findViewById(R.id.phone_number);
         btn=findViewById(R.id.register_button);
         bottomText=findViewById(R.id.bottom_text);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username=edName.getText().toString();
-                String password=edPassword.getText().toString();
-                String ConfirmPassword=edConfirmPassword.getText().toString();
-                String email=edEmail.getText().toString();
-                String Phone_Number=edPhoneNumber.getText().toString();
-                Database db= new  Database(getApplicationContext());
+
+
+                String username=edName.getText().toString().trim();
+                String password=edPassword.getText().toString().trim();
+                String ConfirmPassword=edConfirmPassword.getText().toString().trim();
+                String email=edEmail.getText().toString().trim();
+                String Phone_Number=edPhoneNumber.getText().toString().trim();
+
+
                 if(username.length()==0||password.length()==0||ConfirmPassword.length()==0||email.length()==0||Phone_Number.length()==0)
                 {
                    bottomText.setText("Fill all the field");
@@ -45,8 +56,11 @@ public class Register_Activity extends AppCompatActivity {
                     }
                     else
                     {
-                        db.register(username,password,email,Phone_Number);
-                        bottomText.setText("Already Have an Acoount?");
+                        databaseReference= FirebaseDatabase.getInstance().getReference("Normal_User");
+                        String key = databaseReference.push().getKey();
+                        FirebaseUpload dataStore=new FirebaseUpload(username,password,email,Phone_Number);
+                        databaseReference.child(key).setValue(dataStore);
+                        Toast.makeText(Register_Activity.this, "Successful", Toast.LENGTH_SHORT).show();
                     }
                 }
 
--- app/src/main/java/com/example/myapplication/login_activity.java
+++ app/src/main/java/com/example/myapplication/login_activity.java
@@ -17,7 +17,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class login_activity extends AppCompatActivity {
+
+    DatabaseReference databaseReference;
     EditText edUsername,edPassword;
     Button login;
     TextView register;
@@ -31,20 +36,23 @@ public class login_activity extends AppCompatActivity {
         login=findViewById(R.id.button1);
         register=findViewById(R.id.bolttom);
 
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String name=edUsername.getText().toString();
                 String password=edPassword.getText().toString();
-                Database db= new Database(getApplicationContext());
                 if(name.length()==0||password.length()==0)
                 {
                     register.setText("Please enter login info");
                 }
                 else
                 {
-                    int a=db.login(name,password);
-                    if (a==1)
+
+                    databaseReference= FirebaseDatabase.getInstance().getReference("Normal_User");
+
+
+                    if (1==0)
                     {
                         Toast.makeText(login_activity.this, "Information is ok", Toast.LENGTH_SHORT).show();
                         SharedPreferences sp= getSharedPreferences("Simple_Data", Context.MODE_PRIVATE);
