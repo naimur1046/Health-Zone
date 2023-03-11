2f80be7502237573206ed39ce605d2c67e89c69f
RUMEL <naimurrahman1046@gmail.com>
2023-03-11 14:55:53 +0600

    In this section I have fully completed firebase login and register section

--- app/src/main/java/com/example/myapplication/Register_Activity.java
+++ app/src/main/java/com/example/myapplication/Register_Activity.java
@@ -1,29 +1,43 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.regex.Pattern;
 
 public class Register_Activity extends AppCompatActivity {
     EditText edName,edPassword,edConfirmPassword,edEmail,edPhoneNumber;
     Button btn;
     TextView bottomText;
 
+
     DatabaseReference databaseReference;
+    DatabaseReference databaseReferenceforuser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-
+        databaseReferenceforuser=FirebaseDatabase.getInstance().getReferenceFromUrl("https://health-zone-379113-default-rtdb.firebaseio.com/");
         edName=findViewById(R.id.userName);
         edPassword=findViewById(R.id.password);
         edConfirmPassword=findViewById(R.id.confirm_password);
@@ -50,18 +64,39 @@ public class Register_Activity extends AppCompatActivity {
                 }
                 else
                 {
-                    if(password.compareTo(ConfirmPassword)!=0)
-                    {
-                        bottomText.setText("Check the Password");
-                    }
-                    else
-                    {
-                        databaseReference= FirebaseDatabase.getInstance().getReference("Normal_User");
-                        String key = databaseReference.push().getKey();
-                        FirebaseUpload dataStore=new FirebaseUpload(username,password,email,Phone_Number);
-                        databaseReference.child(key).setValue(dataStore);
-                        Toast.makeText(Register_Activity.this, "Successful", Toast.LENGTH_SHORT).show();
-                    }
+                    databaseReference= FirebaseDatabase.getInstance().getReference("Normal_User");
+                    String key = databaseReference.push().getKey();
+                    FirebaseUpload dataStore=new FirebaseUpload(username,password,email,Phone_Number);
+                    databaseReference.child(key).setValue(dataStore);
+
+
+
+                    databaseReferenceforuser.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot)
+                        {
+
+                            if(snapshot.hasChild(username))
+                            {
+                                Toast.makeText(Register_Activity.this, "User Already Registered", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+
+                                databaseReferenceforuser.child("users").child(username).setValue(password);
+                                finish();
+                            }
+
+                            }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                    Intent intent = new Intent(Register_Activity.this,Home_Activity.class);
+                    startActivity(intent);
+
                 }
 
             }
--- app/src/main/java/com/example/myapplication/login_activity.java
+++ app/src/main/java/com/example/myapplication/login_activity.java
@@ -5,6 +5,7 @@
 package com.example.myapplication;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -17,12 +18,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import okhttp3.internal.cache.DiskLruCache;
 
 public class login_activity extends AppCompatActivity {
 
-    DatabaseReference databaseReference;
+    DatabaseReference databaseReferenceforlogin;
     EditText edUsername,edPassword;
     Button login;
     TextView register;
@@ -31,6 +37,9 @@ public class login_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        databaseReferenceforlogin=FirebaseDatabase.getInstance().getReferenceFromUrl("https://health-zone-379113-default-rtdb.firebaseio.com/");
         edUsername=findViewById(R.id.userName);
         edPassword=findViewById(R.id.confirm_password);
         login=findViewById(R.id.button1);
@@ -49,23 +58,37 @@ public class login_activity extends AppCompatActivity {
                 else
                 {
 
-                    databaseReference= FirebaseDatabase.getInstance().getReference("Normal_User");
+                    databaseReferenceforlogin.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            if (snapshot.hasChild(name))
+                            {
+                                String Get_Password=snapshot.child(name).getValue(String.class);
+                                if(Get_Password.equals(password))
+                                {
+                                    Toast.makeText(login_activity.this, "Information is ok", Toast.LENGTH_SHORT).show();
+                                    SharedPreferences sp= getSharedPreferences("Simple_Data", Context.MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sp.edit();
+                                    editor.putString("User_Name",name);
+                                    editor.putString("Password",password);
+                                    editor.apply();
+                                    startActivity( new Intent(login_activity.this,Home_Activity.class));
+                                }
+                                else
+                                {
+                                    Toast.makeText(login_activity.this, "Your Information is not OK", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
 
 
-                    if (1==0)
-                    {
-                        Toast.makeText(login_activity.this, "Information is ok", Toast.LENGTH_SHORT).show();
-                        SharedPreferences sp= getSharedPreferences("Simple_Data", Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = sp.edit();
-                        editor.putString("User_Name",name);
-                        editor.putString("Password",password);
-                        editor.apply();
-                        startActivity( new Intent(login_activity.this,Home_Activity.class));
-                    }
-                    else
-                    {
-                        Toast.makeText(login_activity.this, "Not ok ", Toast.LENGTH_SHORT).show();
-                    }
                 }
             }
         });
